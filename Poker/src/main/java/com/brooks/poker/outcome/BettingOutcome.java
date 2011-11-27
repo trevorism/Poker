e5a2c0aab0b85d1d74b82927c2f4deaba75eb62f
Trevor Brooks <trevorism@gmail.com>
2011-11-26 22:14:32 -0500

    format

--- Poker/src/main/java/com/brooks/poker/outcome/BettingOutcome.java
+++ Poker/src/main/java/com/brooks/poker/outcome/BettingOutcome.java
@@ -4,12 +4,11 @@ import com.brooks.poker.game.data.GameState;
 import com.brooks.poker.player.Player;
 
 /**
- * @author Trevor 
+ * @author Trevor
  */
 public interface BettingOutcome{
-
+    
     public void modifyGameState(GameState gameState, Player player);
 
     public int getBetAmount(GameState gameState, Player player);
-
 }
