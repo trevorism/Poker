9d07e599e4134803b877b40f6130ee471b3bb169
Trevor Brooks <trevorism@gmail.com>
2012-08-22 07:27:42 -0400

    Make sure each game phase is handled by the game state factory

---
+++ Poker/src/test/java/com/brooks/poker/game/states/GameStateFactoryTest.java
@@ -0,0 +1,31 @@
+package com.brooks.poker.game.states;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.brooks.poker.game.data.GamePhase;
+import com.brooks.poker.game.data.GameState;
+import com.brooks.poker.player.Player;
+import com.brooks.poker.util.PlayerTestSetups;
+import com.brooks.poker.util.PokerTestUtils;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Trevor
+ *
+ */
+public class GameStateFactoryTest{
+
+    @Test
+    public void testInstantiated(){
+        List<Player> players = PlayerTestSetups.allCallingPlayers();
+        GameState gameState = PokerTestUtils.getDefaultGameState(players);
+        GameStateFactory factory = new GameStateFactory(gameState);
+        
+        for(GamePhase phase : GamePhase.values()){
+            assertNotNull(factory.getStateHandler(phase));
+        }
+    }
+}
